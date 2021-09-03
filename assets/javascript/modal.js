8f8c92dbcf5c654a916836f930b4e8a88998dc49
Stephan <stephan-folio@hotmail.fr>
2021-09-03 09:42:17 +0200

    add fonction close modal

--- assets/javascript/modal.js
+++ assets/javascript/modal.js
@@ -1,23 +1,31 @@
+// navigation responsive
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var x = document.getElementById('myTopnav')
+  if (x.className === 'topnav') {
+    x.className += ' responsive'
   } else {
-    x.className = "topnav";
+    x.className = 'topnav'
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector('.bground')
+const modalBtn = document.querySelectorAll('.modal-btn')
+const formData = document.querySelectorAll('.formData')
+const modalClose = document.querySelector('.close')
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal))
+
+// close modal
+modalClose.addEventListener('click', () => {
+  modalbg.style.display = 'none'
+})
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = 'block'
 }
 
-
+console.log(modalClose)
+//
